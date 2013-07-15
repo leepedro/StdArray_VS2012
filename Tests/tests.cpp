e52b8b6d7f554ffbcaeb287c172db432ffd8cd6c
Cheolha Pedro Lee <leepedro@gmail.com>
2013-07-15 05:53:30 -0400

    updated copy()

--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -1,19 +1,75 @@
 #include <array>
 #include <iostream>
+#include <algorithm>
+
+// Enables copy operation only if it is implicitly allowed.
+// 1) integer -> floating && src < dst
+// 2) floating -> floating && src <= dst
+// 2) integer -> integer && {u -> u || s -> s} && src <= dst
+template <typename T, typename U, ::size_t N>
+typename std::enable_if<	
+	(std::is_integral<T>::value && std::is_floating_point<U>::value && sizeof(T) < sizeof(U)) ||
+	(std::is_floating_point<T>::value && std::is_floating_point<U>::value &&
+	sizeof(T) <= sizeof(U)) ||
+	(std::is_integral<T>::value && std::is_integral<U>::value &&
+	((std::is_unsigned<T>::value && std::is_unsigned<U>::value) ||
+	(std::is_signed<T>::value && std::is_signed<U>::value)) &&
+	sizeof(T) <= sizeof(U)), void>::type
+Copy(const std::array<T, N> &src, std::array<U, N> &dst)
+{
+	std::copy(src.cbegin(), src.cend(), dst.begin());
+}
 
 void test_type_conversion(void)
 {
+	// Scalar implicit conversion.
+	float f(0.0f);
+	double d(0.0);
+	int i(0);
+	unsigned int ui(0);
+	long long l(0);
+	unsigned long long ul(0);
+		
+	//f = i;	// warning C4244 "conversion from 'int' to 'float', possible loss of data.
+	d = i;
+	i = ui;		// NO WARNIG. NOT GOOD.
+	ui = i;		// NO WARNIG. NOT GOOD.
+	l = i;
+	//i = l;		// warning C4244 "conversion from '__int64' to 'int', possible loss of data.
+	ul = l;		// NO WARNIG. NOT GOOD.
+	l = ul;		// NO WARNIG. NOT GOOD.
+
 	std::array<int, 3> i1 = {1, 2, 3};
 	std::array<int, 3> i2(i1);		// copy constructor.
 	i2 = i1;						// assignment operator.
 
-	std::array<float, 3> f1 = {11.0, 12.0, 13.0};
+	std::array<double, 3> d1 = {11.0, 12.0, 13.0};
 	//f1 = i1;		// std::array<T> cannot copy each other between different data types.
 
 	// Next line works because of implicit conversion from int scalar to float scalar.
-	std::array<float, 3> f2 = {1, 2, 3};
-	
+	std::array<double, 3> d2 = {21, 22, 23};
+
+	// Next line works by copying each element through the implicit conversion!
+	std::copy(i1.cbegin(), i1.cend(), d1.begin());
+
+	// Next line throws warning C4244 pointing <xutility>
+	// "conversion from 'const float' to 'int', possible loss of data"
+	//std::copy(f2.cbegin(), f2.cend(), i1.begin());
+
+	std::array<unsigned int, 3> ui1 = {4, 5, 6};
+	std::array<float, 3> f1 = {11.0, 12.0, 13.0};
+	std::array<long long, 3> l1 = {101, 102, 103};
+	std::array<unsigned long long, 3> ul1 = {101, 102, 103};
+	//Copy(i1, f1);	// error C2893 "failed to specialize function template"
+	Copy(i1, d1);
+	//Copy(i1, ui1);	// error C2893 "failed to specialize function template" GOOD!
+	//Copy(ui1, i1);	// error C2893 "failed to specialize function template" GOOD!
+	Copy(i1, l1);
+	//Copy(l1, i1);	// error C2893 "failed to specialize function template"
+	//Copy(l1, ul1);	// error C2893 "failed to specialize function template" GOOD!
+	//Copy(ul1, l1);	// error C2893 "failed to specialize function template" GOOD!
 }
+
 
 int main(void)
 {
